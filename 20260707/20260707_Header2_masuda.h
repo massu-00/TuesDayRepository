31abb929abd4f9b3f0a9d05f082a0efc92289d42
massu-00 <tiger.mt@ozzio.jp>
2026-07-14 16:28:26 +0900

    数字あてゲーム

--- 20260707/20260707_Header2_masuda.h
+++ 20260707/20260707_Header2_masuda.h
@@ -1,1 +1,12 @@
 #pragma once
+ void Game();
+ void Random(int& num);
+ void InputCheck(int& num);
+ int Judge(int num, int num2);
+
+
+
+ struct NUM
+ {
+	 int selectnum[10] = { 10 };//初期設定の数字が被らないようにする配列
+ };
--- 20260707/20260707_PracMain2_masuda.cpp
+++ 20260707/20260707_PracMain2_masuda.cpp
@@ -1,11 +1,9 @@
 #include<iostream>
+#include"20260707_Header2_masuda.h"
 using namespace std;
 
 int main()
 {
-	srand((unsigned int)time(NULL));
-
 	Game();
-
 	return 0;
 }
